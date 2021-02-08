3df7b76db24a8c576e51a113aa3777642426f77e
max <dangladste@gmail.com>
2021-02-08 19:14:08 +0200

    # room fix

--- models_impl/src/main/java/com/mdgd/pokemon/models_impl/repo/dao/AppDatabase.kt
+++ models_impl/src/main/java/com/mdgd/pokemon/models_impl/repo/dao/AppDatabase.kt
@@ -2,11 +2,13 @@ package com.mdgd.pokemon.models_impl.repo.dao
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.mdgd.pokemon.models_impl.repo.dao.schemas.MoveSchema
-import com.mdgd.pokemon.models_impl.repo.dao.schemas.PokemonSchema
-import com.mdgd.pokemon.models_impl.repo.schemas.*
+import com.mdgd.pokemon.models.repo.dao.schemas.MoveSchema
+import com.mdgd.pokemon.models.repo.dao.schemas.PokemonSchema
+import com.mdgd.pokemon.models.repo.schemas.*
 
-@Database(entities = [PokemonSchema::class, Ability::class, Form::class, GameIndex::class, MoveSchema::class, Stat::class, Type::class, VersionGroupDetail::class], version = 1, exportSchema = false)
+@Database(version = 1, exportSchema = false,
+        entities = [PokemonSchema::class, Ability::class, Form::class, GameIndex::class,
+            MoveSchema::class, Stat::class, Type::class, VersionGroupDetail::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun pokemonsDao(): PokemonsRoomDao?
 }
--- models_impl/src/main/java/com/mdgd/pokemon/models_impl/repo/network/schemas/PokemonData.kt
+++
@@ -1,6 +0,0 @@
-package com.mdgd.pokemon.models_impl.repo.network.schemas
-
-class PokemonData {
-    var name: String? = null
-    var url: String? = null
-}
