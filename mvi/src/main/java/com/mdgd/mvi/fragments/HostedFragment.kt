d9d7245ac0b80a40ea7528e872440a111fb5632a
DanGdl <dangladste@gmail.com>
2021-11-13 13:39:03 +0200

    # mvi improvement

--- mvi/src/main/java/com/mdgd/mvi/fragments/HostedFragment.kt
+++ mvi/src/main/java/com/mdgd/mvi/fragments/HostedFragment.kt
@@ -2,7 +2,7 @@ package com.mdgd.mvi.fragments
 
 import android.content.Context
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import androidx.lifecycle.*
 import androidx.navigation.fragment.NavHostFragment
 import com.mdgd.mvi.states.ScreenAction
 import com.mdgd.mvi.states.ScreenState
@@ -14,7 +14,7 @@ abstract class HostedFragment<
         ACTION : ScreenAction<VIEW>,
         VIEW_MODEL : FragmentContract.ViewModel<STATE, ACTION>,
         HOST : FragmentContract.Host>
-    : NavHostFragment(), FragmentContract.View, Observer<STATE> {
+    : NavHostFragment(), FragmentContract.View, Observer<STATE>, LifecycleObserver {
 
     protected var model: VIEW_MODEL? = null
         private set
@@ -41,29 +41,28 @@ abstract class HostedFragment<
         fragmentHost = null
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setModel(createModel())
-        if (model != null) {
-            lifecycle.addObserver(model!!)
-            model!!.getStateObservable().observe(this, this)
-            model!!.getActionObservable().observe(this, { action ->
-                action.visit(this as VIEW)
-            })
-        }
+        lifecycle.addObserver(this)
+        model?.getStateObservable()?.observe(this, this)
+        model?.getActionObservable()?.observe(this, { action ->
+            action.visit(this as VIEW)
+        })
     }
 
     protected abstract fun createModel(): VIEW_MODEL
 
-    override fun onDestroy() {
-        // order matters
-        if (model != null) {
-            model!!.getActionObservable().removeObservers(this)
-            model!!.getStateObservable().removeObservers(this)
-            lifecycle.removeObserver(model!!)
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    protected open fun onAny(owner: LifecycleOwner?, event: Lifecycle.Event) {
+        model?.onAny(owner, event)
+
+        if (lifecycle.currentState <= Lifecycle.State.DESTROYED) {
+            lifecycle.removeObserver(this)
+            // order matters
+            model?.getActionObservable()?.removeObservers(this)
+            model?.getStateObservable()?.removeObservers(this)
         }
-        super.onDestroy()
     }
 
     override fun onChanged(screenState: STATE) {
