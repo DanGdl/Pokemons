14f16068a49bff7508de0bd12730576158130da3
YDT_DOM\dan-gl <dan-gl@yit.co.il>
2021-12-13 08:00:15 +0200

    # libs update

--- app/src/main/java/com/mdgd/pokemon/ui/pokemon/dto/ImagePropertyData.kt
+++ app/src/main/java/com/mdgd/pokemon/ui/pokemon/dto/ImagePropertyData.kt
@@ -2,5 +2,5 @@ package com.mdgd.pokemon.ui.pokemon.dto
 
 class ImagePropertyData(override val imageUrl: String) : ImageProperty {
     override val type: Int
-        get() = PokemonProperty.Companion.PROPERTY_IMAGE
+        get() = PokemonProperty.PROPERTY_IMAGE
 }
