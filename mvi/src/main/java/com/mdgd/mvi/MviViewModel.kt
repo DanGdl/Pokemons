3569dddffec9fa38c1d042fa0ad90c64f8795c16
Dan Gld <dangladste@gmail.com>
2022-11-11 12:22:00 +0200

    # libs update

--- mvi/src/main/java/com/mdgd/mvi/MviViewModel.kt
+++ mvi/src/main/java/com/mdgd/mvi/MviViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mdgd.mvi.fragments.FragmentContract
+import com.mdgd.mvi.states.ScreenEffect
 import com.mdgd.mvi.states.ScreenState
 
-abstract class MviViewModel<V, S : ScreenState<V, S>, A> : ViewModel(), FragmentContract.ViewModel<S, A> {
+abstract class MviViewModel<V, S : ScreenState<V, S>> : ViewModel(),
+    FragmentContract.ViewModel<V, S> {
     private val stateHolder = MutableLiveData<S>()
-    private val effectHolder = MutableLiveData<A>()
+    private val effectHolder = MutableLiveData<ScreenEffect<V>>()
 
     override fun getStateObservable() = stateHolder
 
@@ -23,7 +25,7 @@ abstract class MviViewModel<V, S : ScreenState<V, S>, A> : ViewModel(), Fragment
 
     protected fun getState() = stateHolder.value
 
-    protected fun setEffect(action: A) {
+    protected fun setEffect(action: ScreenEffect<V>) {
         effectHolder.value = action
     }
 
