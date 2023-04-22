eec3109834094298f9342eec051df9c1718acb4a
DanDdl <dangladste@gmail.com>
2023-04-22 12:46:58 +0300

    # agp migration to 8.0.0

---
+++ models_impl/src/test/java/com/mdgd/pokemon/models_impl/TestSuit.kt
@@ -0,0 +1,11 @@
+package com.mdgd.pokemon.models_impl
+
+import com.mdgd.pokemon.models_impl.repo.PokemonsRepositoryTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    PokemonsRepositoryTest::class
+)
+class TestSuit
